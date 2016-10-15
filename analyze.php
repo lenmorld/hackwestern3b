6deee887540d3b34d63b37e2f8dc67dcfb6cfd0f
lenmorld <lenmorld@gmail.com>
2016-10-15 13:11:57 -0400

    some working

---
+++ analyze.php
@@ -0,0 +1,13 @@
+<?php
+
+// $str = exec('python ./clarifai.py ' . $query );
+$str = exec('python ./clarifai1.py ');
+
+echo $str;
+
+// $command = escapeshellcmd('./clarifai1.py');
+// $output = shell_exec($command);
+// echo $output;
+?>
+
+
